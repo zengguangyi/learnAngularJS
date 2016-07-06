a440b7ef51315cca06b1b40fc5d09e8b55e23303
zengguangyi <m13680538036@163.com>
2016-07-06 19:20:28 +0800

    npm package test/censorifyGuangyi fixed

--- learnNPM/readwords/readwords.js
+++ learnNPM/readwords/readwords.js
@@ -2,4 +2,7 @@
 *2016.7.6
 *调用自己创建的censorify模块(本地)
 *npm install ../censorify/censorify-0.1.1.tgz
+*npm install censorifyGuangyi(发布到注册表的)
 */
+var censor = require("censorifyGuangyi");
+console.log(censor.getCensoredWords());
